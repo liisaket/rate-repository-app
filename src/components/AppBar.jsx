e120e2cacfe3b05e6eead9ff61975388da99d856
liisa <liisa-m@outlook.com>
2024-08-16 12:44:16 +0300

    10.7: scrollable app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import theme from "../theme";
 import AppBarTab from "./AppBarTab";
@@ -13,7 +13,9 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab />
+      <ScrollView horizontal>
+        <AppBarTab />
+      </ScrollView>
     </View>
   );
 };
